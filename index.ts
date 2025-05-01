120f395bc45b33bbd6b45a5c37d6f1c3c4a6b6e9
f-zamanan <f.n.zamanan@gmail.com>
2025-05-01 16:43:17 +0300

    solved the the full task

--- index.ts
+++ index.ts
@@ -2,43 +2,43 @@
 🎬 Movie Type Definition
 ******************************/
 interface Movie {
-    title: string;
-    director: string;
-    year: number;
-    genre: string;
+  title: string;
+  director: string;
+  year: number;
+  genre: string;
 }
 
 const movies: Movie[] = [
-    {
-        title: "The Shawshank Redemption",
-        director: "Frank Darabont",
-        year: 1994,
-        genre: "Drama",
-    },
-    {
-        title: "The Godfather",
-        director: "Francis Ford Coppola",
-        year: 1972,
-        genre: "Crime",
-    },
-    {
-        title: "Pulp Fiction",
-        director: "Quentin Tarantino",
-        year: 1994,
-        genre: "Crime",
-    },
-    {
-        title: "The Dark Knight",
-        director: "Christopher Nolan",
-        year: 2008,
-        genre: "Action",
-    },
-    {
-        title: "Forrest Gump",
-        director: "Robert Zemeckis",
-        year: 1994,
-        genre: "Drama",
-    },
+  {
+    title: "The Shawshank Redemption",
+    director: "Frank Darabont",
+    year: 1994,
+    genre: "Drama",
+  },
+  {
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: 1972,
+    genre: "Crime",
+  },
+  {
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: 1994,
+    genre: "Crime",
+  },
+  {
+    title: "The Dark Knight",
+    director: "Christopher Nolan",
+    year: 2008,
+    genre: "Action",
+  },
+  {
+    title: "Forrest Gump",
+    director: "Robert Zemeckis",
+    year: 1994,
+    genre: "Drama",
+  },
 ];
 
 /******************************
@@ -49,7 +49,13 @@ and prints the title of each movie.
 printMovieTitles(movies);
 ******************************/
 
+function movieTitle(movies: Movie[]) {
+  movies.forEach((movie) => {
+    console.log("Movie Title: ", movie.title);
+  });
+}
 
+movieTitle(movies);
 
 /******************************
 ✅ Q2) Create a function that accepts:
@@ -59,9 +65,20 @@ and returns how many movies were released in that year.
 // Example usage:
 console.log(countMoviesByYear(movies, 1994)); // Output: 3
 ******************************/
+function countMoviesByYear(movies: Movie[], year: number): number {
+  let filteredArray = movies.filter((movie) => {
+    if (movie.year === year) {
+      return true;
+    }
+  });
 
-
-
+  return movies.length;
+}
+let year: number = 2008;
+console.log(
+  `Number Of movies in the year ${year}:`,
+  countMoviesByYear(movies, year)
+);
 /******************************
 ✅ Q3) Create a function that:
  - accepts an array of movies
@@ -73,3 +90,17 @@ and return the modified array of movies.
 // Example usage:
 const updatedMovies = updateMovieGenre(movies, "Pulp Fiction", "Classic Crime");
 ******************************/
+
+function updateMovieGenre(
+  movies: Movie[],
+  title: string,
+  genre: string
+): Movie[] {
+  let UpdatedMovies = movies.map((movie) => {
+    if (movie.title === title) movie.title = genre;
+    return movie;
+  });
+  return UpdatedMovies;
+}
+const updatedMovies = updateMovieGenre(movies, "Pulp Fiction", "Classic Crime");
+console.log(updatedMovies);
